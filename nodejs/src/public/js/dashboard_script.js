11f547348495ee6b3128df9fd31038b2acbc8f67
BENNY GIL LACTAOTAO <2233116@slu.edu.ph>
2024-12-12 05:03:55 +0800

    fix hamburger on dashboard

--- nodejs/src/public/js/dashboard_script.js
+++ nodejs/src/public/js/dashboard_script.js
@@ -1,3 +1,9 @@
+const hamBurger = document.querySelector("#toggle-btn");
+
+hamBurger.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
+
 document.addEventListener('DOMContentLoaded', async () => {
     const fetchData = async (url) => {
         const response = await fetch(url);
