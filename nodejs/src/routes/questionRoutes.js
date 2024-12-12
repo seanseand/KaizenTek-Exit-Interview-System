34a3477751f311da56f2d836a75f597d5102ad71
Aaron <2233692@slu.edu.ph>
2024-12-12 08:30:22 +0800

    fixed the bug for delete questions in admin side

--- nodejs/src/routes/questionRoutes.js
+++ nodejs/src/routes/questionRoutes.js
@@ -5,7 +5,7 @@ const questionController = require('../controllers/questionController');
 router.get('/get_question', questionController.getQuestion);
 router.post('/addQuestions', questionController.addQuestions);
 router.post('/editQuestions', questionController.editQuestions);
-router.post('/removeQuestions', questionController.removeQuestions);
+router.delete('/removeQuestions', questionController.removeQuestions);
 router.get('/view_questions', questionController.viewQuestions);
 
 module.exports = router;
